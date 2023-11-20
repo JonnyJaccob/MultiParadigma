f460683f55dc8879bca60c4b1273dec35aa13157
Josue <jaccobjosue@gmail.com>
2023-11-20 16:14:42 -0600

    del repositorio

--- Parcial3/Clase0811/app.py
+++ Parcial3/Clase0811/app.py
@@ -1,15 +1,17 @@
 from flask import Flask
 from flask_cors import CORS
 from database import db
-from encriptador import bcrypt  
+from encriptador import bcrypt
 from flask_migrate import Migrate
 from config import BaseConf
 from routes.user.user import appuser
+from routes.imagenes.imagen import imagesUser
 
 app=Flask(__name__)
 app.register_blueprint(appuser)
+app.register_blueprint(imagesUser)
 app.config.from_object(BaseConf)
-CORS(app) 
+CORS(app)
 bcrypt.init_app(app)
 db.init_app(app)
 migrate = Migrate()
--- Parcial3/Clase0811/routes/user/user.py
+++ Parcial3/Clase0811/routes/user/user.py
@@ -1,42 +1,46 @@
 from flask import Blueprint,request,jsonify,render_template,redirect
-from sqlalchemy import exc
+from sqlalchemy import exc 
 from models import User
 from app import db,bcrypt
-from auth import tokenCheck, verificar
+from auth import tokenCheck,verificar
 
 
-appuser = Blueprint('appuser',__name__,template_folder='templates')
+appuser=Blueprint('appuser',__name__,template_folder="templates")
 
-@appuser.route("/auth/registro",methods = ["POST"])
+@appuser.route("/")
+def index():
+    return render_template("index.html")
+
+@appuser.route("/auth/registro",methods=["POST"])
 def registro():
-    user  = request.get_json()
-    userExists = User.query.filter_by(email=user["email"]).first()
+    user = request.get_json()
+    userExists=User.query.filter_by(email=user['email']).first()
     if not userExists:
-        usuario = User(email=user['email'],password=user['password'])
+        usuario=User(email=user['email'],password=user['password'])
         try:
             db.session.add(usuario)
             db.session.commit()
             mensaje="Usuario Creado"
         except exc.SQLAlchemyError as e:
-            mensaje= "Error: " + e
-    return jsonify({"Mensaje" : mensaje})
+            mensaje="ERROR "+e
+    return jsonify({"message":mensaje})
 
-@appuser.route("/auth/login",methods=["POST"])
+@appuser.route('/auth/login',methods=["POST"])
 def login():
     user = request.get_json()
     usuario = User(email=user['email'],password=user['password'])
-    searchUser = User.query.filter_by(email = usuario.email).first
+    searchUser = User.query.filter_by(email=usuario.email).first()
     if searchUser:
-        validation = bcrypt.check_password_hash(searchUser.password,user)
+        validation = bcrypt.check_password_hash(searchUser.password,user["password"])
         if validation:
-            auth_token = usuario.encode_auth_token(user_id=searchUser)
+            auth_token=usuario.encode_auth_token(user_id=searchUser.id)
             response = {
                 'status':'success',
                 'message':'Login exitoso',
                 'auth_token':auth_token
             }
             return jsonify(response)
-    return jsonify({"message":'Datos incorrectos'})
+    return jsonify({"message":"Datos incorrectos"})
 
 @appuser.route('/usuarios',methods=['GET'])
 @tokenCheck
@@ -45,80 +49,79 @@ def getUsers(usuario):
     print(usuario['admin'])
     if usuario['admin']:
         output=[]
-        usuarios = User.query.all()
+        usuarios=User.query.all()
         for usuario in usuarios:
-            usuarioData = {}
-            usuarioData['id'] = usuario.id
-            usuarioData['email'] = usuario.email
-            usuarioData['password'] = usuario.password
-            usuarioData['registered_on'] = usuario.registered_on
+            usuarioData={}
+            usuarioData['id']=usuario.id
+            usuarioData['email']=usuario.email
+            usuarioData['password']=usuario.password
+            usuarioData['registered_on']=usuario.registered_on
             output.append(usuarioData)
-        return jsonify({'usuario':output})
+        return jsonify({'usuarios':output})
     else:
-        return jsonify('token no encontrado')
+        return jsonify({'Error':"No tienes permisos"})
     
 @appuser.route('/main')
 def main():
     return render_template('main.html')
 
-@appuser.route('login',methods= ["GET","POST"])
+@appuser.route('/login',methods=["GET","POST"])
 def login_post():
-    if(request.method == "GET"):
+    if(request.method=="GET"):
         token = request.args.get('token')
         if token:
             info = verificar(token)
-            if(info['status'] == "fail"):
-                responseObject = {
+            if(info['status']!="fail"):
+                responseObject={
                     'status':"success",
                     'message':'valid token',
                     'info':info
                 }
                 return jsonify(responseObject)
         return render_template('login.html')
-    else: 
-        email: request.json['email']
-        password = request.json['password']
+    else:
+        email =request.json['email']
+        password=request.json['password']
+        print(request.json)
         usuario = User(email=email,password=password)
         searchUser = User.query.filter_by(email=email).first()
         if searchUser:
             validation = bcrypt.check_password_hash(searchUser.password,password)
             if validation:
                 auth_token = usuario.encode_auth_token(user_id=searchUser.id)
-                responseObject = {
-                    'status':'success',
+                responseObject={
+                    'status':"success",
                     'login':'Loggin exitoso',
                     'auth_token':auth_token
                 }
                 return jsonify(responseObject)
-        return jsonify({'message':'Datos incorrectos'})
-                
-@appuser.route('/sign',methods = ["GET","POST"])
-def login_post():
-    if request.method == "GET":
+        return jsonify({'message':"Datos incorrectos"})
+    
+@appuser.route('/sign',methods=["GET","POST"])
+def logint_post():
+    if request.method=="GET":
         return render_template('register.html')
     else:
-        email = request.json['email']
-        password = request.json['password']
+        email=request.json['email']
+        password=request.json['password']
         usuario = User(email=email,password=password)
         userExists = User.query.filter_by(email=email).first()
         if not userExists:
             try:
                 db.session.add(usuario)
                 db.session.commit()
-                responseObject = {
+                responseObject={
                     'status':'success',
-                    'message':'Registro exitoso'
+                    'message':"Registro exitoso"
                 }
             except exc.SQLAlchemyError as e:
-                db.session.add(usuario)
-                db.session.commit()
-                responseObject = {
+                responseObject={
                     'status':'error',
-                    'message': e
+                    'message':e
                 }
         else:
-            responseObject = {
+            responseObject={
                 'status':'error',
-                'message':"usuario existente"
+                'message':'usuario existente'
             }
         return jsonify(responseObject)
